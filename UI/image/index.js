90749aec354bec3e1b4ef9bddd5621baee4aa6e6
Hogonov <43898789+Hogonov@users.noreply.github.com>
2025-01-05 10:44:24 +0500

    Refactor image handling and update dependencies.
    Replaced custom base64 utility for image blurring with a more streamlined approach using `useEffect` for dynamic image source handling. Deleted `.idea` project files and upgraded dependencies in `package.json` and `package-lock.json` to improve compatibility and performance, including the addition of `sharp`.

--- UI/image/index.js
+++ UI/image/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import baseStyles from './index.module.scss';
 import Image from 'next/image';
 import pic404 from '@/public/404.jpg';
 
 export const CustomImage = (props) => {
-    const   {
+    const {
         src,
         width = 300,
         height = 200,
@@ -13,31 +13,29 @@ export const CustomImage = (props) => {
         quality,
     } = props;
 
-    const keyStr =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    const [processedSrc, setProcessedSrc] = useState(src);
 
-    const triplet = (e1, e2, e3) =>
-        keyStr.charAt(e1 >> 2) +
-    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-    keyStr.charAt(e3 & 63);
+    useEffect(() => {
+        if (typeof src === 'string') {
+            setProcessedSrc(src);
+        } else if (typeof src === 'object') {
+            setProcessedSrc(src);
+        } else {
+            setProcessedSrc(pic404);
+        }
+    }, [src]);
 
-    const rgbDataURL = (r, g, b) =>
-        `data:image/jpg;base64,R0lGODlhAQABAPAA${
-            triplet(255, r, g) + triplet(b, 255, 255)
-        }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
-
-    const [srcLocal, setSrcLocal] = useState(src);
-
-    return <Image className={`${baseStyles.main} ${styles}`}
-        src={srcLocal}
-        width={width}
-        height={height}
-        alt={alt}
-        onError={() => setSrcLocal(pic404)}
-        quality={quality}
-        placeholder="blur"
-        blurDataURL={rgbDataURL(114, 114, 114)}
-        {...props}
-    />;
+    return (
+        <Image
+            className={`${baseStyles.main} ${styles}`}
+            src={processedSrc}
+            width={width}
+            height={height}
+            alt={alt}
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,..."
+            quality={quality}
+            {...props}
+        />
+    );
 };
